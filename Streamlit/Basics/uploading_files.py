4780464f1ac854bd123a869d5c4587d7f1e91e9d
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-06-23 09:14:34 +0530

    file uploader function

--- Streamlit/Basics/uploading_files.py
+++ Streamlit/Basics/uploading_files.py
@@ -4,7 +4,21 @@ import pandas as pd
 
 
 st.title('Upload Files')
+st.subheader('Image')
+uploaded_file = st.file_uploader(label='Upload an Image',type='jpg')
+st.write(uploaded_file.size) 
 
-uploaded_file = st.file_uploader(label='Upload an Image')
+st.image(image=uploaded_file)
+
+
+st.subheader('Video')
+uploaded_file = st.file_uploader(label='Upload an Image',type='mp4')
 st.write(uploaded_file) 
+
+st.video(data=uploaded_file)
+
+
+
+
+
 
