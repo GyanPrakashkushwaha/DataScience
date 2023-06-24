cb375346b794b23bb3c374eb25ca3c437b8e7822
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-06-24 09:53:56 +0530

    src

--- Streamlit/Basics/Interactive_Widgets.py
+++ Streamlit/Basics/Interactive_Widgets.py
@@ -29,3 +29,44 @@ st.subheader('Multiple Select Box')
 multiselectBox = st.multiselect(label='what is your favourite color?(Multiple Select Box)',options=['green ','blue','yello','black','read'])
 print(multiselectBox)
 st.write(multiselectBox)
+
+
+
+# More functions
+
+st.title('More functions')
+
+st.subheader('Slider.')
+slider =st.slider('I am a slider')
+print(slider)
+st.write(slider)
+
+st.subheader('Slider with more functions')
+slider =st.slider('I am a slider',min_value=50,max_value=500,value=200)
+print(slider)
+st.write(slider)
+
+
+# st.title(' INput')
+# st.subheader('camera')
+# camera = st.camera_input(label='camera')
+# if camera:
+#     st.image(camera)
+#     print(camera)
+
+st.subheader('text')
+txt = st.text_input('input',autocomplete='hello')
+if txt:
+    st.write(txt)
+
+
+st.subheader('text area')
+txt = st.text_area('input')
+if txt:
+    st.write(txt)
+
+st.subheader('Date input')
+txt = st.date_input('input')
+if txt:
+    st.write(txt)
+
--- Streamlit/Basics/uploading_files.py
+++ Streamlit/Basics/uploading_files.py
@@ -6,16 +6,18 @@ import pandas as pd
 st.title('Upload Files')
 st.subheader('Image')
 uploaded_file = st.file_uploader(label='Upload an Image',type='jpg')
-st.write(uploaded_file.size) 
 
-st.image(image=uploaded_file)
+if uploaded_file is not None:
+    st.write(uploaded_file.size) 
+    st.image(image=uploaded_file)
 
 
 st.subheader('Video')
 uploaded_file = st.file_uploader(label='Upload an Image',type='mp4')
-st.write(uploaded_file) 
 
-st.video(data=uploaded_file)
+if uploaded_file is not None:
+    st.write(uploaded_file.size) 
+    st.video(data=uploaded_file)
 
 
 
