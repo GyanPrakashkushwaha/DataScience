7f1638fdb6ee4a66caf487e2bdec8336ecbbc63a
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-31 12:18:00 +0530

    fast api

---
+++ fastAPI/main.py
@@ -0,0 +1,22 @@
+from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+conn = MongoClient()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
