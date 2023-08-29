e3e4c5b60cd6c4d576771d962be7419cf6c9de8a
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-08-29 16:26:48 +0530

    add gradio tutorial

---
+++ Others/gradio/basic.py
@@ -0,0 +1,10 @@
+import gradio as gr
+
+def image_classifier(inp):
+    return {'cat': 0.3, 'dog': 0.7}
+
+demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
+demo.launch(share=True)
+
+
+
