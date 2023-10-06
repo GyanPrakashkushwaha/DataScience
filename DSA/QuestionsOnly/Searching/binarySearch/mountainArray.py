c4e934ee05aee724c7394680f74a08c016c45c15
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-10-06 08:44:11 +0530

    mountain index

--- DSA/QuestionsOnly/Searching/binarySearch/mountainArray.py
+++ DSA/QuestionsOnly/Searching/binarySearch/mountainArray.py
@@ -1,30 +1,23 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: list[int]) -> int:
-        self.array = arr
 
-        ans = self.findPeak()
-        return ans
-    
-
-    def findPeak(self):
         start = 0
-        end = len(self.array)
+        end = len(arr)
 
         while start < end:
             mid = start + (end-start)//2
 
-            if self.array[mid] < self.array[mid+1]:
+            if arr[mid] < arr[mid+1]:
                 start = mid+1
             else:
                 end = mid
 
-        return self.array[start]
+        return start
     
-
         
     
 obj = Solution()
-obj.peakIndexInMountainArray([0,10,5,2])
+print(obj.peakIndexInMountainArray([0,10,5,2]))
 
     
 
